59ceae041c5726973005529e8411f0052b6635a7
Phillip Willcocks <pwil139@aucklanduni.ac.nz>
2020-09-17 12:27:22 +1200

    animated opening

--- resources/views/home/open.blade.php
+++ resources/views/home/open.blade.php
@@ -1,2 +1,22 @@
+<style>
+    #open-img {
+        transition: transform 10s;
 
-<img src="{{env('AWS_URL').'/'. session('product')->image }}" >
+    }
+    #open-img:hover {
+        transform: rotateY(1180deg);
+}
+    #open-img:active {
+
+}
+</style>
+<script>
+    document.getElementById('open-img').addEventListener('click', (e) => {
+        e.target.src = {{env('AWS_URL').'/'. session('product')->image }};
+    })
+</script>
+
+
+<img id="open-img" src="https://prizedrive.s3-ap-southeast-2.amazonaws.com/uploads/box.jpg" >
+
+
