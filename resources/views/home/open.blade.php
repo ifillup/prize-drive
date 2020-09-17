b418629b57a7baf4c5837ff41d62cbd88dc5f56b
Phillip Willcocks <pwil139@aucklanduni.ac.nz>
2020-09-17 19:09:56 +1200

    animated opening

--- resources/views/home/open.blade.php
+++ resources/views/home/open.blade.php
@@ -12,13 +12,15 @@
 </style>
 
 
-
+<div class="">
+    <p>Click to open!!!</p>
 <img id="open-img" class='rotate' src="https://prizedrive.s3-ap-southeast-2.amazonaws.com/uploads/box.jpg" >
-
+</div>
 
 <script>
     document.getElementById('open-img').addEventListener('click', (e) => {
         e.target.classList.remove('rotate');
         e.target.src = "{{env('AWS_URL').'/'. session('product')->image }}"
+        e.target.innerText = "You won a {{session('product')->name}}!!!!"
     })
 </script>
