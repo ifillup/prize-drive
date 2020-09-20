2861a03e604bbfe5d74bac2511ddff9257f64442
Phillip Willcocks <pwil139@aucklanduni.ac.nz>
2020-09-21 11:32:27 +1200

    styling

--- resources/views/home/feature.blade.php
+++ resources/views/home/feature.blade.php
@@ -1,4 +1,4 @@
-<div class="" >
+<div class="feature" >
     <p>Buy and a box and it will show here</p>
 <img id="open-img" class='rotate' src="https://prizedrive.s3-ap-southeast-2.amazonaws.com/uploads/box.jpg" >
 </div>
