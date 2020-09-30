a1166827f941c13cfd9cedce4b7ba412cc9fe0c5
Phillip Willcocks <pwil139@aucklanduni.ac.nz>
2020-09-30 13:07:59 +1300

    updates

--- resources/views/home/box.blade.php
+++ resources/views/home/box.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+<div class="card  pt-8">
+    <img src="{{env('AWS_URL').'/'.$box->image}}" alt="" srcset="">
+</div>
+</div>
+<div class="card-list">
+@foreach ($box->prizes as $prize)
+    <img src="{{env('AWS_URL').'/'.$prize->product->image}}" alt="" srcset="">
+@endforeach
+</div>
 
-{{$box}}
-{{$box->prizes}}
+
 @endsection
