e499d6865c832690e41a55471001924251024bd5
Phillip Willcocks <pwil139@aucklanduni.ac.nz>
2020-10-19 11:44:01 +1300

    admin status

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -26,9 +26,9 @@
         <td>
             <form action="{{ secure_url('/admin/toggle') }}" method="POST">
                 @csrf
-                @method('PATCH');
+                @method('PATCH')
                     <input type="hidden" name="user_id" value="{{$user->id}}" >
-                    <button class="btn btn-primary btn-sm" type="submit" >{{$user->admin ? 'Demote' : 'Admin'}}</button>
+                    <button class="btn btn-primary btn-sm " {{$user->admin ? 'disabled' : ''}} type="submit" >{{$user->admin ? 'Demote' : 'Admin'}}</button>
                 </form>
         </td>
         <td>{{$user->transactions->sum('value')}}</td>
