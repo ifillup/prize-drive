d4b434fa0931311ed6b80f78ba0cfbd47f49564b
Phillip Willcocks <pwil139@aucklanduni.ac.nz>
2020-10-21 11:06:44 +1300

    caseinsensitive filenames

---
+++ app/Inventory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventory extends Model
+{
+    protected $guarded = [];
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
